0bbefb159222aed05a0fc0b4eccf5c28f9eee6f7
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2024-02-25 15:43:22 -0300

    commenting not understood OEM table

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -15,26 +15,26 @@ start: jmp loader
 ;***********************************
 ;   OEM Parameter Block
 ;***********************************
-bpbOEM                  db "Hashida "
+;; bpbOEM                  db "Hashida "
 
-bpbBytesPerSector:      DW 512
-bpbSectorsPerCluster:   DB 1
-bpbReservedSectors:     DW 1
-bpbNumberOfFATs:        DB 2
-bpbRootEntries:         DW 224
-bpbTotalSectors:        DW 2880
-bpbMedia:               DB 0xF0
-bpbSectorsPerFAT:       DW 9
-bpbSectorsPerTrack:     DW 18
-bpbHeadsPerCylinder:    DW 2
-bpbHiddenSectors:       DD 0
-bpbTotalSectorsBig:     DD 0
-bsDriveNumber:          DB 0
-bsUnused:               DB 0
-bsExtBootSignature:     DB 0x29
-bsSerialNumber:         DD 0xa0a1a2a3
-bsVolumeLabel:          DB "MOS FLOPPY "
-bsFileSystem:           DB "FAT12   "
+;; bpbBytesPerSector:      DW 512
+;; bpbSectorsPerCluster:   DB 1
+;; bpbReservedSectors:     DW 1
+;; bpbNumberOfFATs:        DB 2
+;; bpbRootEntries:         DW 224
+;; bpbTotalSectors:        DW 2880
+;; bpbMedia:               DB 0xF0
+;; bpbSectorsPerFAT:       DW 9
+;; bpbSectorsPerTrack:     DW 18
+;; bpbHeadsPerCylinder:    DW 2
+;; bpbHiddenSectors:       DD 0
+;; bpbTotalSectorsBig:     DD 0
+;; bsDriveNumber:          DB 0
+;; bsUnused:               DB 0
+;; bsExtBootSignature:     DB 0x29
+;; bsSerialNumber:         DD 0xa0a1a2a3
+;; bsVolumeLabel:          DB "MOS FLOPPY "
+;; bsFileSystem:           DB "FAT12   "
 
 ;******************************
 ; Prints a string
@@ -44,7 +44,7 @@ print:
     lodsb
     or al,al
     jz printDone
-    mov ah, 0eh
+    mov ah, 0x0e
     int 10h
     jmp print
 
